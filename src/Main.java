e248f9c177240018351fa49774974fb8dbc9d8bd
kekich119 <patowach3o@gmail.com>
2023-09-21 23:12:25 +0300

    3 нагрузки

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String gymArr [] = {"На ноги", "На руки", "На пресс"};
+        String lengGym [] = {"Приседы", "что то ещё"}; //доделать
+        String pres [] = {"Скручивание", "Поднятие ног на турнике"};
+        String hend [] = {"Подтягивание", "Отжимание"};
+     }
+}
